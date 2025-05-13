b7bda5d14229b3302e427a7af673fac23ed636d1
Luis Isaac Cuautle Delgadillo <cuautle.tron@gmail.com>
2025-05-13 14:41:16 -0600

    mostrando las bebidas

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,7 @@ import z from "zod";
 
 import {
   CategoriesAPIResponseShcema,
+  DrinkAPIResponse,
   DrinksAPIResponses,
   SearchFilterSchema,
 } from "../schemas/recipies-schema";
@@ -9,3 +10,4 @@ import {
 export type Categories = z.infer<typeof CategoriesAPIResponseShcema>;
 export type SearchFilters = z.infer<typeof SearchFilterSchema>;
 export type Drinks = z.infer<typeof DrinksAPIResponses>;
+export type Drink = z.infer<typeof DrinkAPIResponse>;
